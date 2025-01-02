84fe2eecd7a30b0cb9875911240fcf0fe5cc5ae4
uwovx <uwovx@student.kit.edu>
2025-01-02 20:09:00 +0100

    finished language libraries for loginpage, signup page and password recovery

--- frontentdummy/src/components/register/InteractionScreen.tsx
+++ frontentdummy/src/components/register/InteractionScreen.tsx
@@ -1,19 +1,21 @@
 import GuestButton from "../buttons/GuestButton";
 import InputField from "../buttons/input";
 import PrimButton from "../buttons/PrimButton";
+import { useTranslation } from "react-i18next";
 
 const InteractionScreen = () => {
+  const { t } = useTranslation();
   return (
     <div className="absolute öeft-8 top-10 w-6/12">
       <h1 className="text-3xl font-medium">Account Erstellen</h1>
       <br></br>
       <div className="grid gap-6 mb-6 md:grid-cols-1">
-        <InputField title="Benutzername" placeHolder="Tano" />
-        <InputField title="E-Mail Adresse" placeHolder="tano@example.de" />
-        <InputField title="Passwort" placeHolder="**********" />
-        <InputField title="Passwort wiederholen" placeHolder="**********" />
+        <InputField title={t("userName")} placeHolder="Tano" />
+        <InputField title={t("e-mail_adress")} placeHolder="tano@example.de" />
+        <InputField title={t("password")} placeHolder="**********" />
+        <InputField title={t("forgot_password")} placeHolder="**********" />
       </div>
-      <PrimButton content="Registrieren" route="/overview" />
+      <PrimButton content={t("register")} route="/overview" />
       <br></br>
       <br></br>
       <p className="text-center">
